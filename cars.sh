72db26a2079c2765cfe81a9e5fd66498f96d0766
Grant Sielman <69483686+gsielman@users.noreply.github.com>
2020-12-10 23:42:37 -0800

    Add files via upload

---
+++ cars.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+# cars.sh
+#Grant Sielman
+chmod 755
+echo "Enter a number 1-3"
+while [ "$n" -le "number" ]
+	echo "Enter a 1 to add a new car to the list"
+	echo "Enter a 2 to display the list of cars"
+	echo "Enter a 3 to quit the program"
+case $number in
+	1)
+		y = echo "Enter the year the car was made: "
+		b = echo "Enter the make of the car: "
+		m = echo "Enter the model of car: "
+		c = "${y}:{b}:{m}"
+		echo "${c}"
+		$cat 1 >> My_old_cars
+case $number in
+	2)
+		source My_old_cars
+case $number in
+	3)
+		echo "Goodbye."
+		break
