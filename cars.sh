6618f0187313169aabb5a48a09b6e6ec57f7fe7b
Grant Sielman <69483686+gsielman@users.noreply.github.com>
2020-12-11 10:39:29 -0800

    Update cars.sh

--- cars.sh
+++ cars.sh
@@ -3,19 +3,21 @@
 #Grant Sielman
 chmod 755
 echo "Enter a number 1-3"
-while [ "$n" -le "number" ]
+n = 1
+number = 1, 2, 3
+while [ "$n" ! a \> b "number" ]
 do
 	echo "Enter a 1 to add a new car to the list"
 	echo "Enter a 2 to display the list of cars"
 	echo "Enter a 3 to quit the program"
 case $number in
 	1)
-		y = echo "Enter the year the car was made: "
-		b = echo "Enter the make of the car: "
-		m = echo "Enter the model of car: "
-		c = "${y}:{b}:{m}"
+		y=$(echo "Enter the year the car was made: ")
+		b=$(echo "Enter the make of the car: ")
+		m=$(echo "Enter the model of car: ")
+		c="${y}:{b}:{m}"
 		echo "${c}"
-		$cat 1 >> My_old_cars
+		$(cat 1 >> My_old_cars)
 		;;
 	2)
 		source My_old_cars
